83941d3c5d64065e4c3d5f1e33b99d9110cfe914
Ricardo <ricardo.csantos0877@gmail.com>
2022-03-17 00:00:59 -0300

    feat: footer and fix mobile nav scrolling

--- menu.js
+++ menu.js
@@ -23,13 +23,18 @@ function responsiveMenu () {
 
 function navbar() {
   var prevScrollpos = window.pageYOffset;
+  let menu = document.querySelector('.menu')
+
   window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.querySelector("nav").style.top = "0";
+    if (menu.className == 'menu open-menu') {
     } else {
-      document.querySelector("nav").style.top = "-100px";
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.querySelector("nav").style.top = "0";
+      } else {
+        document.querySelector("nav").style.top = "-100px";
+      }
+      prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
   }
 }
